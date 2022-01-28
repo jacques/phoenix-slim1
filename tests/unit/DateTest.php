4a01da4cbab206a7945f70537402e6f4090ef886
Jacques Marneweck <jacques@siberia.co.za>
2022-01-28 21:50:22 +0200

    Fix test

--- tests/unit/DateTest.php
+++ tests/unit/DateTest.php
@@ -1,16 +1,16 @@
 <?php declare(strict_types=1);
 /**
  * @author    Jacques Marneweck <jacques@siberia.co.za>
- * @copyright 2018-2019 Jacques Marneweck.  All rights strictly reserved.
+ * @copyright 2018-2022 Jacques Marneweck.  All rights strictly reserved.
  */
 
 namespace Jacques\Phoenix\Tests\Unit;
 
 use Carbon\Carbon;
 
-class DateTest extends \PHPUnit\Framework\TestCase
+final class DateTest extends \PHPUnit\Framework\TestCase
 {
-    public function testZeroDate()
+    public function testZeroDate(): void
     {
         $result = \Jacques\Phoenix\Date::dateAfterMinOrMin('0000-00-00 00:00:00', '2014-01-01 00:00:00', 'Y-m-d H:i:s');
 
@@ -18,7 +18,7 @@ class DateTest extends \PHPUnit\Framework\TestCase
         self::assertEquals($expected, $result);
     }
 
-    public function testJan1970()
+    public function testJan1970(): void
     {
         $result = \Jacques\Phoenix\Date::dateAfterMinOrMin('1970-01-01 00:00:00', '2014-01-01 00:00:00', 'Y-m-d H:i:s');
 
@@ -26,12 +26,12 @@ class DateTest extends \PHPUnit\Framework\TestCase
         self::assertEquals($expected, $result);
     }
 
-    public function testNullDate()
+    public function testNullDate(): void
     {
-        $result = \Jacques\Phoenix\Date::dateAfterMinOrMin(null, '2014-01-01 00:00:00', 'Y-m-d H:i:s');
-
         $knownDate = Carbon::create(2019, 3, 1);
         Carbon::setTestNow($knownDate);
+
+        $result = \Jacques\Phoenix\Date::dateAfterMinOrMin(null, '2014-01-01 00:00:00', 'Y-m-d H:i:s');
 
         $expected = Carbon::createFromFormat('Y-m-d H:i:s', '2019-03-01 00:00:00');
         self::assertEquals($expected, $result);
