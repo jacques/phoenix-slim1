311441336b055657db2c85e6af08988ffc12bd20
Jacques Marneweck <jacques@siberia.co.za>
2022-01-30 11:24:55 +0200

    Make class final

--- src/HTTP.php
+++ src/HTTP.php
@@ -1,12 +1,37 @@
 <?php declare(strict_types=1);
 /**
+ *         ##### ##      /
+ *      ######  /###   #/                                       #
+ *     /#   /  /  ###  ##                                      ###
+ *    /    /  /    ### ##                                       #
+ *        /  /      ## ##
+ *       ## ##      ## ##  /##      /###     /##  ###  /###   ###     /##    ###
+ *       ## ##      ## ## / ###    / ###  / / ###  ###/ #### / ###   / ###  #### /
+ *     /### ##      /  ##/   ###  /   ###/ /   ###  ##   ###/   ##      ### /###/
+ *    / ### ##     /   ##     ## ##    ## ##    ### ##    ##    ##       ##/  ##
+ *       ## ######/    ##     ## ##    ## ########  ##    ##    ##        /##
+ *       ## ######     ##     ## ##    ## #######   ##    ##    ##       / ###
+ *       ## ##         ##     ## ##    ## ##        ##    ##    ##      /   ###
+ *       ## ##         ##     ## ##    ## ####    / ##    ##    ##     /     ###
+ *       ## ##         ##     ##  ######   ######/  ###   ###   ### / /       ### /
+ *  ##   ## ##          ##    ##   ####     #####    ###   ###   ##/ /         ##/
+ * ###   #  /                 /
+ *  ###    /                 /
+ *   #####/                 /
+ *     ###                 /
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
  * @author    Jacques Marneweck <jacques@siberia.co.za>
- * @copyright 2018 Jacques Marneweck.  All rights strictly reserved.
+ * @copyright 2018-2022 Jacques Marneweck.  All rights strictly reserved.
+ * @package   Phoenix
  */
 
 namespace Jacques\Phoenix;
 
-class HTTP
+final class HTTP
 {
     /**
      * Download file.
